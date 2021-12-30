f00230eca7edc95f0aa30b2509245faf2de9b3eb
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2021-12-30 22:46:00 +0000

    now node.js version

--- README.md
+++ README.md
@@ -17,23 +17,24 @@ $ gh extension install crguezl/gh-org-clone
 ## Usage
 
 ```txt
-gh org-clone [-t TOPIC] [-s QUERY] [-p PATH] [-y] ORG
-  ORG
-    Github organization. Required if the GITHUB_ORG environment variable is not set.
-  -y, --yes
-    Clone without prompting for confirmation.
-  -p, --path PATH
-    Clone path. Default: current directory.
-  -t, --topic TOPIC
-    Clone repositories with this topic
-  -s --search QUERY
-    Clone repositories found by this search string. If this is provided '-t' will be ignored.
-    Example: -s "is:public language:go"
-    See: https://docs.github.com/en/github/searching-for-information-on-github/searching-on-github/searching-for-repositories
-  -n, --dry-run
-    Do not actually clone, just show what would be cloned
-  -h, --help
-    Display this message.
+Usage: gh org-clone [options] [organization] [options]
+
+Options:
+  -V, --version                              output the version number
+  -s, --search <query>                       search <query> using GitHub Search API. A dot '.' refers to all the repos
+  -r, --regexp <regexp>                      filter <query> results using <regexp>
+  -c, --csr <comma separated list of repos>  the list of repos is specified as a comma separated list
+  -f, --file <file>                          file with the list of repos, one per line
+  -n --dryrun                                just show what repos will be added as submodules
+  -o --org <org>                             default organization or user
+  -D --depth <depth>                         Create a shallow clone with a history truncated to <depth> number of commits
+  -p --parallel <int>                        number of concurrent  processes during the cloning stage (default: 2)
+  -h, --help                                 display help for command
+
+  - If the organization is not explicitly specified the selection will be done interactively among the list of your organizations
+  - You can set the default organization through the GITHUB_ORG environment variable
+  - If no repos are specified the selection of repos will be done interactively among the repos in the org
+  - Option '-s' assumes all the repos belong to the same org
 ```
 
 ## Examples of use
@@ -62,124 +63,3 @@ ULL-ESIT-DMSI-1920/pb-gh-campus-expert-fuegonellaa
 ULL-ESIT-DMSI-1920/p1-t1-iaas-fuegonellaa
 ```
 
-### How it works 
-
-To obtain the repos `gh-org-clone` makes a request like:
-
-```
-$ gh api --paginate /search/repositories?q=org%3AULL-ESIT-DMSI-1920%20fuegonella
-{
-    "total_count": 3,
-    "incomplete_results": false,
-    "items": [
-        {
-            "id": 411615845,
-            "node_id": "R_kgDOGIjCZQ",
-            "name": "markdown-fuegonellaa",
-            "full_name": "ULL-ESIT-DMSI-1920/markdown-fuegonellaa",
-            "private": false,
-            "owner": { ... },
-            ...
-            "score": 1.0
-        },
-        {
-            ...
-            "name": "pb-gh-campus-expert-fuegonellaa",
-            "full_name": "ULL-ESIT-DMSI-1920/pb-gh-campus-expert-fuegonellaa",
-            "private": false,
-            "owner": { ... },
-            ...
-            "score": 1.0
-        },
-        {
-            ...
-            "full_name": "ULL-ESIT-DMSI-1920/p1-t1-iaas-fuegonellaa",
-            "private": true,
-            "owner": { ...  },
-            ...
-            "score": 1.0
-        }
-    ]
-}
-```
-
-### jq to the rescue
-
-The json is very large and contains a lot of information!
-
-We can make use of `jq` to filter the results
-(See [jq](https://ull-esit-dmsi-1920.github.io/tema1-introduccion/jq)): 
-
-```
-[~/campus-virtual/2021/learning/gh-learning/gh-org-clone(master)]$ gh api --paginate /search/repositories?q=org%3AULL-ESIT-DMSI-1920%20fuegonella | jq .items[].full_name -
-"ULL-ESIT-DMSI-1920/markdown-fuegonellaa"
-"ULL-ESIT-DMSI-1920/pb-gh-campus-expert-fuegonellaa"
-"ULL-ESIT-DMSI-1920/p1-t1-iaas-fuegonellaa"
-```
-
-We can make very complex querys with `jq`:
-
-```
-$ gh api --paginate /search/repositories?q=org%3AULL-ESIT-DMSI-1920%20fuegonella | \
-  jq '.items[] | .full_name, .private, .commits_url'
-"ULL-ESIT-DMSI-1920/markdown-fuegonellaa"
-false
-"https://api.github.com/repos/ULL-ESIT-DMSI-1920/markdown-fuegonellaa/commits{/sha}"
-"ULL-ESIT-DMSI-1920/pb-gh-campus-expert-fuegonellaa"
-false
-"https://api.github.com/repos/ULL-ESIT-DMSI-1920/pb-gh-campus-expert-fuegonellaa/commits{/sha}"
-"ULL-ESIT-DMSI-1920/p1-t1-iaas-fuegonellaa"
-true
-"https://api.github.com/repos/ULL-ESIT-DMSI-1920/p1-t1-iaas-fuegonellaa/commits{/sha}"
-```
-
-Here is an example using the commits URL of one of the repos:
-
-```
-$ gh api "https://api.github.com/repos/ULL-ESIT-DMSI-1920/markdown-fuegonellaa/commits" | \
-      jq '.[].commit | (.author | .date, .name), .message'
-"2021-09-29T10:11:05Z"
-"Antonella García"
-"Practica 1"
-"2021-09-29T09:46:08Z"
-"github-classroom[bot]"
-"Add online IDE url"
-```
-
-Fortunately, `gh` has a `--jq` option that does the same that the former pipe example.
-
-But first, let avoid the `gh` default paginator:
-
-
-```
-[~/campus-virtual/2021/learning/gh-learning/gh-org-clone(master)]$ gh  config set pager cat
-[~/campus-virtual/2021/learning/gh-learning/gh-org-clone(master)]$ gh  config get pager
-cat
-```
-
-And here it is the output of `gh` using `--jq`:
-
-```
-[~/campus-virtual/2021/learning/gh-learning/gh-org-clone(master)]$ gh api --paginate /search/repositories?q=org%3AULL-ESIT-DMSI-1920%20fuegonella --jq .items[].full_name 
-ULL-ESIT-DMSI-1920/markdown-fuegonellaa
-ULL-ESIT-DMSI-1920/pb-gh-campus-expert-fuegonellaa
-ULL-ESIT-DMSI-1920/p1-t1-iaas-fuegonellaa
-```
-
-### What happens if one of the repos already exists
-
-If the repo already exists the script 
-attempts to pull the last version of the default branch
-
-```
-~/campus-virtual/2021/learning/gh-learning/gh-org-clone(master)]$ ./gh org-clone -s markdown-fuegonellaa -y -p tmp/
-Retrieving the list of repositories: QUERY=search/repositories?q=org%3AULL-ESIT-DMSI-1920%20markdown-fuegonellaa JQ_QUERY=.items[].full_name
-Cloning 1 repositories to tmp/...
-ULL-ESIT-DMSI-1920/markdown-fuegonellaa
-ULL-ESIT-DMSI-1920
-markdown-fuegonellaa
-'markdown-fuegonellaa' already exists, attempting to checkout the default branch and pull
-Ya en 'main'
-Tu rama está actualizada con 'origin/main'.
-Ya está actualizado.
-```
---
+++ gh-org-clone.js
@@ -0,0 +1,254 @@
+const ins = require("util").inspect;
+const deb = (...args) => { 
+    if (debug) console.log(ins(...args, {depth: null})); 
+};
+
+const path = require('path');
+const fs = require("fs");
+const shell = require('shelljs');
+
+const { 
+  showError, 
+  //sh, 
+  //shContinue, shStderr, gh, 
+  //ghCont, 
+  //ghCode, 
+  names2urls, 
+  //getUserLogin,
+  //getRepoListFromFile, 
+  //getRepoListFromAPISearch,
+  //getNumberOfCommits,
+  //branches,
+  numBranches,
+  //RepoIsEmpty,
+  getRepoList,
+  fzfGetOrg,
+  //addImplicitOrgIfNeeded,
+  addSubmodules
+} = require(path.join(__dirname,'utilities.js'));
+
+const { Command } = require('commander');
+
+const program = new Command();
+program.version(require('./package.json').version);
+
+program
+  .name("gh org-clone [options] [organization]")
+  .option('-s, --search <query>', "search <query> using GitHub Search API. A dot '.' refers to all the repos")
+  .option('-r, --regexp <regexp>', 'filter <query> results using <regexp>')
+  .option('-c, --csr <comma separated list of repos>', 'the list of repos is specified as a comma separated list')
+  .option('-f, --file <file>', 'file with the list of repos, one per line')
+  .option('-n --dryrun','just show what repos will be added as submodules')
+  .option('-o --org <org>', 'default organization or user')
+  .option('-D --depth <depth>','Create a shallow clone with a history truncated to <depth> number of commits')
+  .option('-p --parallel <int>', 'number of concurrent  processes during the cloning stage', 2);
+
+program.addHelpText('after', `
+  - If the organization is not explicitly specified the selection will be done interactively among the list of your organizations
+  - You can set the default organization through the GITHUB_ORG environment variable
+  - If no repos are specified the selection of repos will be done interactively among the repos in the org 
+  - Option '-s' assumes all the repos belong to the same org`
+);
+  
+program.parse(process.argv);
+//if (process.argv.length === 2) program.help()
+
+const debug = program.debug; 
+
+const options = program.opts();
+// console.log(options.search || options.file || options.csr);
+// console.log(program.args)
+
+if (!(options.search || options.file || options.csr)) options.search = '.';
+
+if (!shell.which('git')) {
+  showError('Sorry, this extension requires git installed!');
+}
+if (!shell.which('gh')) {
+    showError('Sorry, this extension requires GitHub Cli (gh) installed!');
+}
+
+debugger;
+if (!options.org && (program.args.length == 1) ) options.org = program.args.shift();
+
+let org = options.org ||  process.env["GITHUB_ORG"] || fzfGetOrg();
+
+let repos = getRepoList(options, org);
+
+if (repos.length === 0) {
+  console.log(`No matching repos found in owner "${org}"!`);
+  process.exit(0);
+}
+
+if (options.regexp) {
+    let regexp = new RegExp(options.regexp,'i');
+    repos = repos.filter(rn => {
+      return regexp.test(rn)
+    });
+}
+
+if (options.dryrun) {
+
+    //console.error("Only repos with more than one commit will be added as submodules:")
+
+    //console.log(repos);
+    let nb = numBranches(repos)
+    repos.forEach((r,i) => {
+      if (nb[i] === 0)
+        console.error(`${r} is empty!`);
+      else console.log(r);
+    });
+
+    process.exit(0);
+}
+
+let urls = names2urls(repos);
+
+addSubmodules(urls, repos, options.parallel, options.depth, true, program.args);
+
+/*
+
+######### UNIFIED graphQL NO parallelism in the downloads of repos
+
+# unified graphql request for empty repos download complete of 1 repo
+➜  asyncserialize git:(master) ✗ time gh submodule-add -s 'asyncserialize' --org ULL-MII-SYTWS-2122
+Skipping to add repo https://github.com/ULL-MII-SYTWS-2122/asyncserialize-lorenaolaru because is empty!
+Skipping to add repo https://github.com/ULL-MII-SYTWS-2122/asyncserialize-mstoisor because is empty!
+Skipping to add repo https://github.com/ULL-MII-SYTWS-2122/asyncserialize-crguezl because is empty!
+Skipping to add repo https://github.com/ULL-MII-SYTWS-2122/asyncserialize-ddialar because is empty!
+git submodule add https://github.com/ULL-MII-SYTWS-2122/asyncserialize-alu0101102726
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/sytws2122/practicas-alumnos/asyncserialize/asyncserialize-alu0101102726'...
+Skipping to add repo https://github.com/ULL-MII-SYTWS-2122/asyncserialize-PaulaExposito because is empty!
+gh submodule-add -s 'asyncserialize' --org ULL-MII-SYTWS-2122  0,99s user 0,43s system 36% cpu 3,946 total
+
+# unified graphql request for empty repos -n option. No downloads
+➜  asyncserialize git:(master) ✗ time gh submodule-add -s asyncserialize -o ULL-MII-SYTWS-2122 -n
+Only repos with more than one commit will be added as submodules:
+ULL-MII-SYTWS-2122/asyncserialize-mstoisor is empty!
+ULL-MII-SYTWS-2122/asyncserialize-lorenaolaru is empty!
+ULL-MII-SYTWS-2122/asyncserialize-crguezl is empty!
+ULL-MII-SYTWS-2122/asyncserialize-ddialar is empty!
+ULL-MII-SYTWS-2122/asyncserialize-alu0101102726
+ULL-MII-SYTWS-2122/asyncserialize-PaulaExposito is empty!
+gh submodule-add -s asyncserialize -o ULL-MII-SYTWS-2122 -n  0,39s user 0,15s system 26% cpu 2,040 total
+
+# unified graphql request for empty repos -n option. 16 repos. No downloads
+➜  aprender-markdown git:(master) ✗ time gh submodule-add -s aprender-markdown --org ULL-MFP-AET-2122 -n
+Only repos with more than one commit will be added as submodules:
+ULL-MFP-AET-2122/aprender-markdown-anabel-coello-perez-alu0100885200
+ULL-MFP-AET-2122/aprender-markdown-alejandro-gonzalez-sarasola-alu0100260076
+ULL-MFP-AET-2122/aprender-markdown-wim-van-hoye-alu0101520377
+ULL-MFP-AET-2122/aprender-markdown-adela-gonzalez-maury-alu0101116204
+ULL-MFP-AET-2122/aprender-markdown-ivan-gonzalez-aguiar-alu0100551266
+ULL-MFP-AET-2122/aprender-markdown-carlos-guerra-olivera-alu0100703535
+ULL-MFP-AET-2122/aprender-markdown-adrian-prieto-curbelo_alu0100948387
+ULL-MFP-AET-2122/aprender-markdown-manuel_curbelo_alu0100045130
+ULL-MFP-AET-2122/aprender-markdown-chloe-boistel-perez-alu0100788020
+ULL-MFP-AET-2122/aprender-markdown-noelia-rodriguez-hernandez-alu0100595420
+ULL-MFP-AET-2122/aprender-markdown-alejandro-marrero-diaz-alu100825008
+ULL-MFP-AET-2122/aprender-markdown-yeray_exposito_garcia_alu0100951844
+ULL-MFP-AET-2122/aprender-markdown-angel-ramallobenitez-alu0100312898
+ULL-MFP-AET-2122/aprender-markdown-nestor-gonzalez-lopez-alu0100108859
+ULL-MFP-AET-2122/aprender-markdown-jaime-garcia-bullejos-alu0100906806
+ULL-MFP-AET-2122/gitpod-template
+gh submodule-add -s aprender-markdown --org ULL-MFP-AET-2122 -n  0,40s user 0,16s system 26% cpu 2,122 total
+
+# Unified graphql request for empty repos -n option. 16 downloads
+
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-anabel-coello-perez-alu0100885200
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-anabel-coello-perez-alu0100885200'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-alejandro-gonzalez-sarasola-alu0100260076
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-alejandro-gonzalez-sarasola-alu0100260076'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-adela-gonzalez-maury-alu0101116204
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-adela-gonzalez-maury-alu0101116204'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-wim-van-hoye-alu0101520377
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-wim-van-hoye-alu0101520377'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-ivan-gonzalez-aguiar-alu0100551266
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-ivan-gonzalez-aguiar-alu0100551266'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-adrian-prieto-curbelo_alu0100948387
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-adrian-prieto-curbelo_alu0100948387'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-manuel_curbelo_alu0100045130
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-manuel_curbelo_alu0100045130'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-carlos-guerra-olivera-alu0100703535
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-carlos-guerra-olivera-alu0100703535'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-chloe-boistel-perez-alu0100788020
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-chloe-boistel-perez-alu0100788020'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-noelia-rodriguez-hernandez-alu0100595420
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-noelia-rodriguez-hernandez-alu0100595420'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-alejandro-marrero-diaz-alu100825008
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-alejandro-marrero-diaz-alu100825008'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-angel-ramallobenitez-alu0100312898
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-angel-ramallobenitez-alu0100312898'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-nestor-gonzalez-lopez-alu0100108859
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-nestor-gonzalez-lopez-alu0100108859'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-yeray_exposito_garcia_alu0100951844
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-yeray_exposito_garcia_alu0100951844'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-jaime-garcia-bullejos-alu0100906806
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-jaime-garcia-bullejos-alu0100906806'...
+git submodule add https://github.com/ULL-MFP-AET-2122/gitpod-template
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/gitpod-template'...
+gh submodule-add -s aprender-markdown --org ULL-MFP-AET-2122  4,05s user 2,63s system 27% cpu 24,702 total
+
+### Sequential
+
+# 16 repos no downloads
+➜  aprender-markdown git:(master) ✗ time gh submodule-add -s 'aprender-markdown' --org ULL-MFP-AET-2122 -n
+Only repos with more than one commit will be added as submodules:
+ULL-MFP-AET-2122/aprender-markdown-anabel-coello-perez-alu0100885200
+ULL-MFP-AET-2122/aprender-markdown-adela-gonzalez-maury-alu0101116204
+ULL-MFP-AET-2122/aprender-markdown-alejandro-gonzalez-sarasola-alu0100260076
+ULL-MFP-AET-2122/aprender-markdown-wim-van-hoye-alu0101520377
+ULL-MFP-AET-2122/aprender-markdown-adrian-prieto-curbelo_alu0100948387
+ULL-MFP-AET-2122/aprender-markdown-manuel_curbelo_alu0100045130
+ULL-MFP-AET-2122/aprender-markdown-carlos-guerra-olivera-alu0100703535
+ULL-MFP-AET-2122/aprender-markdown-noelia-rodriguez-hernandez-alu0100595420
+ULL-MFP-AET-2122/aprender-markdown-ivan-gonzalez-aguiar-alu0100551266
+ULL-MFP-AET-2122/aprender-markdown-chloe-boistel-perez-alu0100788020
+ULL-MFP-AET-2122/aprender-markdown-alejandro-marrero-diaz-alu100825008
+ULL-MFP-AET-2122/aprender-markdown-angel-ramallobenitez-alu0100312898
+ULL-MFP-AET-2122/aprender-markdown-nestor-gonzalez-lopez-alu0100108859
+ULL-MFP-AET-2122/aprender-markdown-yeray_exposito_garcia_alu0100951844
+ULL-MFP-AET-2122/aprender-markdown-jaime-garcia-bullejos-alu0100906806
+ULL-MFP-AET-2122/gitpod-template
+gh submodule-add -s 'aprender-markdown' --org ULL-MFP-AET-2122 -n  2,33s user 0,83s system 26% cpu 11,815 total
+
+➜  aprender-markdown git:(master) ✗ time gh submodule-add -s 'aprender-markdown' --org ULL-MFP-AET-2122
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-anabel-coello-perez-alu0100885200
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-anabel-coello-perez-alu0100885200'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-alejandro-gonzalez-sarasola-alu0100260076
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-alejandro-gonzalez-sarasola-alu0100260076'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-wim-van-hoye-alu0101520377
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-wim-van-hoye-alu0101520377'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-adela-gonzalez-maury-alu0101116204
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-adela-gonzalez-maury-alu0101116204'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-ivan-gonzalez-aguiar-alu0100551266
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-ivan-gonzalez-aguiar-alu0100551266'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-carlos-guerra-olivera-alu0100703535
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-carlos-guerra-olivera-alu0100703535'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-adrian-prieto-curbelo_alu0100948387
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-adrian-prieto-curbelo_alu0100948387'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-manuel_curbelo_alu0100045130
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-manuel_curbelo_alu0100045130'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-chloe-boistel-perez-alu0100788020
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-chloe-boistel-perez-alu0100788020'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-noelia-rodriguez-hernandez-alu0100595420
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-noelia-rodriguez-hernandez-alu0100595420'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-alejandro-marrero-diaz-alu100825008
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-alejandro-marrero-diaz-alu100825008'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-yeray_exposito_garcia_alu0100951844
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-yeray_exposito_garcia_alu0100951844'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-angel-ramallobenitez-alu0100312898
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-angel-ramallobenitez-alu0100312898'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-nestor-gonzalez-lopez-alu0100108859
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-nestor-gonzalez-lopez-alu0100108859'...
+git submodule add https://github.com/ULL-MFP-AET-2122/aprender-markdown-jaime-garcia-bullejos-alu0100906806
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/aprender-markdown-jaime-garcia-bullejos-alu0100906806'...
+git submodule add https://github.com/ULL-MFP-AET-2122/gitpod-template
+Clonando en '/Users/casianorodriguezleon/campus-virtual/2122/aet2122/practicas-alumnos/aprender-markdown/gitpod-template'...
+gh submodule-add -s 'aprender-markdown' --org ULL-MFP-AET-2122  5,96s user 3,25s system 27% cpu 33,934 total
+
+
+## Parallel clone and graphql unified
+
+16 repos gh submodule-add -s aprender-markdown --org ULL-MFP-AET-2122  4,30s user 2,87s system 57% cpu 12,540 total
+*/
